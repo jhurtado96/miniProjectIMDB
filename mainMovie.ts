6858bde25ea6bd5fa88efa6059a9284a39c3e4cc
NataliSb9 <nsainzbarroso@gmail.com>
2021-02-18 14:06:17 +0100

    MainMovie final

--- mainMovie.ts
+++ mainMovie.ts
@@ -1,43 +1,57 @@
 import{Movie} from "./Movie"
 import { Professional } from "./Professional"
-//profesionales
-let profesionalcito1 = new Professional("Pablo",33,"Masculino",68,1.83,"Rubio","Azul","Blanco",false,"Espanyol",3,"Actor")
-let profesionalcito2 = new Professional("Luis",43,"Masculino",88,1.89,"Catanyo","Marron","Negro",true,"Noruego",3,"Manager")
-let profesionalcito3 = new Professional("Julian",53,"Masculino",98,1.53,"Pelirrojo","Verde","Amarillo",false,"Espanyol",3,"Producer")
-let profesionalArr = [profesionalcito1,profesionalcito2,profesionalcito3]
-//Movies
+
+//Movie 1
 let movie1 = new Movie("Cadena Perpetua", 1994, "EEUU", "drama");
-let movie2 = new Movie("The gentlemen", 2020, "UK", "accion");
-let movie3 = new Movie("Soul", 2020, "EEUU", "animacion");
-let movie4 = new Movie("La trinchera infinita", 2019, "España", "drama");
-let movie5 = new Movie("Vengadores: infinity war", 2018, "EEUU", "accion")
+//profesionales movie1
+let director1= new Professional("Frank Darabont",33,"Masculino",68,1.83,"Rubio","Azul","Blanco",false,"Estadounidense",3,"Director")
+let distributor1= new Professional("Luis",43,"Masculino",88,1.89,"Catanyo","Marron","Negro",true,"Noruego",1,"distribuidor")
+let writer1 = new Professional("Stephen King",53,"Masculino",98,1.93,"Pelirrojo","Verde","Amarillo",false,"Espanyol",2,"guionista")
+let producer1 = new Professional ("John Steward",53,"Masculino",75,1.73,"Pelirrojo","Verde","Amarillo",false,"Espanyol",0,"productor")
+let actor1Movie1 = new Professional("Tim Robbins",62,"Masculino",70,1.75,"Castanyo","Verde","Blanco",false,"estadounidense",3,"actor")
+let actor2Movie1 = new Professional("Morgan Freeman",83,"Masculino",81,1.90,"Gris","Marron","Negro",false,"Estadounidense",1,"actor")
+let actors1 = [actor1Movie1,actor2Movie1]
 
+//Movie2
+let movie2 = new Movie("Vengadores: infinity war", 2018, "EEUU", "accion")
+//Profesionales Movie2
+let director2= new Professional("Anthony Russo",43,"Masculino",78,1.83,"Moreno","Verde","Blanco",false,"Estadounidense",2,"Director")
+let distributor2= new Professional("Rose Evans",43,"Femenino",64,1.55,"Pelirroja","Marron","Negra",true,"Estadounidense",0,"distribuidor")
+let writer2 = new Professional("Christopher Markus",48,"Masculino",76,1.77,"Pelirrojo","Verde","Amarillo",false,"Canadiense",3,"guionista")
+let producer2= new Professional ("Trench Ruffalo",73,"Masculino",70,1.73,"rubio","Verde","Amarillo",false,"Ingles",3,"productor")
+let actor1Movie2 = new Professional("Robert Downey Jr",55,"Masculino",71,1.73,"Moreno","Marron","Blanco",false,"Estadounidense",0,"actor")
+let actor2Movie2 = new Professional("Chris Evans",39,"Masculino",82,1.91,"Rubio","Azul","Blanco",false,"Estadounidense",0,"actor")
 
-//Metodos set
-movie1.setActors(profesionalArr);
-movie1.setWriter(profesionalcito2);
-movie1.setDistributor(profesionalcito3);
-movie1.setDirector(profesionalcito2);
-movie1.setProducer(profesionalcito1)
+let actors2 = [actor1Movie2,actor2Movie2]
+
+//Metodos set movie 1
+movie1.setActors(actors1);
+movie1.setWriter(writer1);
+movie1.setDistributor(distributor1);
+movie1.setDirector(director1);
+movie1.setProducer(producer1)
 movie1.setGenre("drama");
-movie1.setIsMCU(true);
+movie1.setIsMCU(false);
 movie1.setPlataform("Netflix");
-
-//Mostrar metodos
+movie1.setNationality("EEUU y Canada");
+movie1.setLanguaje("ingles");
+movie1.setMainCharacterName("Andrew Dufresne")
 movie1.mostrarDatosPeliculas();
-/*movie2.mostrarDatosPeliculas();
-movie3.mostrarDatosPeliculas();
-movie4.mostrarDatosPeliculas();
-movie5.mostrarDatosPeliculas();
 
-console.log("Metodos Get")
-console.log(movie3.getReleaseYear());
-console.log(movie2.getDirector());
-console.log(movie2.getWriter());
-console.log(movie1.getDistributor());
-console.log(movie1.getGenre());
-console.log(movie1.getTitle());
-console.log(movie2.getTitle());*/
+
+//Metodos set movie 2
+movie2.setActors(actors2);
+movie2.setWriter(writer2);
+movie2.setDistributor(distributor2);
+movie2.setDirector(director2);
+movie2.setProducer(producer2)
+movie2.setGenre("acción y drama");
+movie2.setIsMCU(true);
+movie2.setPlataform("Disney");
+movie2.setNationality("EEUU");
+movie2.setLanguaje("ingles");
+movie2.setMainCharacterName("Tony Stark")
+movie2.mostrarDatosPeliculas();
 
 
 
