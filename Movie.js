ba4187bc67e6fc3f8838242bee54a8a8432ee203
jhurtado96 <javierhurtado98@gmail.com>
2021-02-18 10:42:29 +0100

    actualizaPeliculas

--- Movie.js
+++ Movie.js
@@ -8,11 +8,100 @@ var Movie = /** @class */ (function () {
         this.nationality = nationality;
         this.genre = genre;
     }
+    Movie.prototype.getTitle = function () {
+        return this.title;
+    };
+    Movie.prototype.setTitle = function (title) {
+        this.title = title;
+    };
+    Movie.prototype.getReleaseYear = function () {
+        return this.releaseYear;
+    };
+    Movie.prototype.setReleaseYear = function (releaseYear) {
+        this.releaseYear = releaseYear;
+    };
+    Movie.prototype.getActors = function () {
+        return this.actors;
+    };
+    Movie.prototype.setActors = function (actors) {
+        this.actors = actors;
+    };
+    Movie.prototype.getNationality = function () {
+        return this.nationality;
+    };
+    Movie.prototype.setNationality = function (nationality) {
+        this.nationality = nationality;
+    };
+    Movie.prototype.getDirector = function () {
+        return this.director;
+    };
+    Movie.prototype.setDirector = function (director) {
+        this.director = director;
+    };
+    Movie.prototype.getWriter = function () {
+        return this.writer;
+    };
+    Movie.prototype.setWriter = function (writer) {
+        this.writer = writer;
+    };
+    Movie.prototype.getLanguaje = function () {
+        return this.languaje;
+    };
+    Movie.prototype.setLanguaje = function (languaje) {
+        this.languaje = languaje;
+    };
+    Movie.prototype.getPlataform = function () {
+        return this.plataform;
+    };
+    Movie.prototype.setPlataform = function (plataform) {
+        this.plataform = plataform;
+    };
+    Movie.prototype.isIsMCU = function () {
+        return this.isMCU;
+    };
+    Movie.prototype.setIsMCU = function (isMCU) {
+        this.isMCU = isMCU;
+    };
+    Movie.prototype.getMainCharacterName = function () {
+        return this.mainCharacterName;
+    };
+    Movie.prototype.setMainCharacterName = function (mainCharacterName) {
+        this.mainCharacterName = mainCharacterName;
+    };
+    Movie.prototype.getProducer = function () {
+        return this.producer;
+    };
+    Movie.prototype.setProducer = function (producer) {
+        this.producer = producer;
+    };
+    Movie.prototype.getDistributor = function () {
+        return this.distributor;
+    };
+    Movie.prototype.setDistributor = function (distributor) {
+        this.distributor = distributor;
+    };
+    Movie.prototype.getGenre = function () {
+        return this.genre;
+    };
+    Movie.prototype.setGenre = function (genre) {
+        this.genre = genre;
+    };
     Movie.prototype.mostrarDatosPeliculas = function () {
         console.log("Peliculas: ");
         console.log("Tittle: " + this.title);
         console.log("Release year: " + this.releaseYear);
-        console.log("");
+        for (var i = 0; i < this.getActors().length; i++) {
+            if (this.getActors()[i].getProfession() == "actor") {
+                console.log("Actors: " + this.getActors()[i].mostrarDatos);
+            }
+        }
+        console.log("Nacionality: " + this.nationality);
+        console.log("Director: " + this.director);
+        console.log("Writer: " + this.writer);
+        console.log("Language: " + this.languaje);
+        console.log("Plataform: " + this.plataform);
+        console.log("Is MCU?: " + this.isMCU);
+        console.log();
     };
     return Movie;
 }());
--- Movie.ts
+++ Movie.ts
@@ -15,6 +15,111 @@ export class Movie{
     private distributor:Professional
     private genre:string
 
+    public getTitle(): string {
+        return this.title;
+    }
+
+    public setTitle(title: string): void {
+        this.title = title;
+    }
+
+    public getReleaseYear(): number {
+        return this.releaseYear;
+    }
+
+    public setReleaseYear(releaseYear: number): void {
+        this.releaseYear = releaseYear;
+    }
+
+    public getActors(): Professional[] {
+        return this.actors;
+    }
+
+    public setActors(actors: Professional[]): void {
+        this.actors = actors;
+    }
+
+    public getNationality(): string {
+        return this.nationality;
+    }
+
+    public setNationality(nationality: string): void {
+        this.nationality = nationality;
+    }
+
+    public getDirector(): Professional {
+        return this.director;
+    }
+
+    public setDirector(director: Professional): void {
+        this.director = director;
+    }
+
+    public getWriter(): Professional {
+        return this.writer;
+    }
+
+    public setWriter(writer: Professional): void {
+        this.writer = writer;
+    }
+
+    public getLanguaje(): string {
+        return this.languaje;
+    }
+
+    public setLanguaje(languaje: string): void {
+        this.languaje = languaje;
+    }
+
+    public getPlataform(): string {
+        return this.plataform;
+    }
+
+    public setPlataform(plataform: string): void {
+        this.plataform = plataform;
+    }
+
+    public isIsMCU(): boolean {
+        return this.isMCU;
+    }
+
+    public setIsMCU(isMCU: boolean): void {
+        this.isMCU = isMCU;
+    }
+
+    public getMainCharacterName(): string {
+        return this.mainCharacterName;
+    }
+
+    public setMainCharacterName(mainCharacterName: string): void {
+        this.mainCharacterName = mainCharacterName;
+    }
+
+    public getProducer(): Professional {
+        return this.producer;
+    }
+
+    public setProducer(producer: Professional): void {
+        this.producer = producer;
+    }
+
+    public getDistributor(): Professional {
+        return this.distributor;
+    }
+
+    public setDistributor(distributor: Professional): void {
+        this.distributor = distributor;
+    }
+
+    public getGenre(): string {
+        return this.genre;
+    }
+
+    public setGenre(genre: string): void {
+        this.genre = genre;
+    }
+
+
     constructor(tittle:string,releaseYear:number,nationality:string,genre:string){
         this.title=tittle
         this.releaseYear=releaseYear
@@ -26,7 +131,19 @@ export class Movie{
         console.log("Peliculas: ")
         console.log("Tittle: "+ this.title)
         console.log("Release year: "+this.releaseYear)
-        console.log("")
+        for(let i = 0; i<this.getActors().length;i++){
+            if(this.getActors()[i].getProfession()=="actor"){
+                console.log("Actors: " + this.getActors()[i].mostrarDatos)
+            }
+        }
+        console.log("Nacionality: "+this.nationality)
+        console.log("Director: " + this.director)
+        console.log("Writer: " + this.writer)
+        console.log("Language: " + this.languaje)
+        console.log("Plataform: " +this.plataform)
+        console.log("Is MCU?: " +this.isMCU)
+        console.log()
+        
     }
 
 
