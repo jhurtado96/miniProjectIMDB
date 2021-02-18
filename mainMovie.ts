ab264e38355f3178e548226725eb07d8c94b1d33
NataliSb9 <nsainzbarroso@gmail.com>
2021-02-18 11:02:18 +0100

    mainMovie con objetos creados

--- mainMovie.ts
+++ mainMovie.ts
@@ -1,1 +1,14 @@
 import{Movie} from "./Movie"
+import { Professional } from "./Professional"
+
+let movie1 = new Movie("Cadena Perpetua", 1994, "EEUU", "drama");
+let movie2 = new Movie("The gentlemen", 2020, "UK", "accion");
+let movie3 = new Movie("Soul", 2020, "EEUU", "animacion");
+let movie4 = new Movie("La trinchera infinita", 2019, "España", "drama");
+let movie5 = new Movie("Vengadores: infinity war", 2018, "EEUU", "accion")
+
+movie1.mostrarDatosPeliculas();
+movie2.mostrarDatosPeliculas();
+movie3.mostrarDatosPeliculas();
+movie4.mostrarDatosPeliculas();
+movie5.mostrarDatosPeliculas();
